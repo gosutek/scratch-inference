5aedb6f2c80dbc5b76c0a7c0f1f896312c8c0275
GosuTek <gosutek@gmail.com>
2026-06-29 12:47:46 +0300

    improve k_input_embeddings by respecting memory coalescing

--- src/cuda/kernels/input_embeddings.cu
+++ src/cuda/kernels/input_embeddings.cu
@@ -6,13 +6,7 @@
  * and keep an archive of older versions
 **/
 
-/*
-  * +------------------------------------------------------------------------------+
-  * |                               VERSION 1                                      |
-  * +------------------------------------------------------------------------------+
-*/
-
-__global__ void k_fetch_input_embeddings(
+__global__ void k_fetch_input_embeddings_v1(
 	const u32* const __restrict__ input_tokens,
 	const u64 input_tokens_len,
 	const u32 dim,
@@ -24,4 +18,17 @@ __global__ void k_fetch_input_embeddings(
 	_d_dn_rm_set(input_embeddings, dim, blockIdx.x, threadIdx.x * stride + 1, _d_dn_rm_get(embeddings_table, dim, input_tokens[blockIdx.x], threadIdx.x * stride + 1));
 	_d_dn_rm_set(input_embeddings, dim, blockIdx.x, threadIdx.x * stride + 2, _d_dn_rm_get(embeddings_table, dim, input_tokens[blockIdx.x], threadIdx.x * stride + 2));
 	_d_dn_rm_set(input_embeddings, dim, blockIdx.x, threadIdx.x * stride + 3, _d_dn_rm_get(embeddings_table, dim, input_tokens[blockIdx.x], threadIdx.x * stride + 3));
+}
+
+__global__ void k_fetch_input_embeddings(
+	const u32* const __restrict__ input_tokens,
+	const u64 input_tokens_len,
+	const u32 dim,
+	const bf16* const __restrict__ embeddings_table,
+	bf16* const __restrict__ input_embeddings)
+{
+	for (i32 i = threadIdx.x; i < dim; i += blockDim.x) {
+		const bf16 a = _d_dn_rm_get(embeddings_table, dim, input_tokens[blockIdx.x], i);
+		_d_dn_rm_set(input_embeddings, dim, blockIdx.x, i, a);
+	}
 }
--- src/cuda/kernels/input_embeddings.cuh
+++ src/cuda/kernels/input_embeddings.cuh
@@ -2,6 +2,18 @@
 
 #include "cuda_helpers.cuh"
 
+/**
+ * This kernel gets its own separate file
+ * such that I can later improve on it
+ * and keep an archive of older versions
+**/
+
+/*
+  * +------------------------------------------------------------------------------+
+  * |                               VERSION 1                                      |
+  * +------------------------------------------------------------------------------+
+*/
+
 __global__ void k_fetch_input_embeddings(
 	const u32* const __restrict__ input_tokens,
 	const u64 input_tokens_len,
